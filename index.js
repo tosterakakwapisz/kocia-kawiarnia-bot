5b4eb7e0129db93799e5beb89ae0086af5d160a8
TheToster <kasper.knapik@gmail.com>
2022-04-10 17:08:08 +0200

    index.js - intents, import commands and events handler

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-import { Client, Intents } from 'discord.js';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, Intents } = require('discord.js');
+const info = require('./commands/info');
+const ping = require('./commands/ping');
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+    Intents.FLAGS.GUILD_SCHEDULED_EVENTS
+  ]
+});
 const config = require('./config.json');
+const onGuildMemberAdd = require('./events/onGuildMemberAdd.js');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+client.on('guildMemberAdd', onGuildMemberAdd);
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+client.on('messageCreate', async msg => {
+  let command = msg.content.split(' ')[0];
+  switch (command) {
+    case `${config.prefix}ping`: ping(msg); break;
+    case `${config.prefix}info`: info(msg); break;
   }
 });
 
